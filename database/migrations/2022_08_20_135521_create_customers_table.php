235ffb7bf2cc1a0fce32fac442be0ef28f13d134
derlisrd <derlisruizdiaz@hotmail.com>
2022-08-20 14:15:34 -0400

    migration

---
+++ database/migrations/2022_08_20_135521_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('site_id')->nullable();
+            $table->string("doc",100)->unique();
+            $table->string("name",100);
+            $table->string('lastname',100);
+            $table->string('tel');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
