2e7ad4cd12b09e79f63ff7b277d49aed9be0eaae
derlisrd <derlisruizdiaz@hotmail.com>
2026-01-15 15:18:38 -0300

    feat: Add login components and layout
    - Created a new login component in resources/views/components/auth/login.blade.php.
    - Established a new layout in resources/views/layouts/app.blade.php for consistent page structure.
    - Developed the login page in resources/views/pages/login.blade.php with form elements and branding.
    - Removed the empty login view in resources/views/public/login.blade.php.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,21 @@
-<div>
-
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+     @fluxAppearance
+</head>
+<body>
+    
+    <flux:button>Default</flux:button>
+<flux:button variant="primary">Primary</flux:button>
+<flux:button variant="filled">Filled</flux:button>
+<flux:button variant="danger">Danger</flux:button>
+<flux:button variant="ghost">Ghost</flux:button>
+<flux:button variant="subtle">Subtle</flux:button>
+    @fluxScripts
+</body>
+</html>
